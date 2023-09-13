536dc376864e1f78cb4387df4bc23e184457264c
김범창 <tigerspear.kim@gmail.com>
2023-09-13 14:04:00 +0900

    create backend project

---
+++ backend/src/test/java/com/windows33/Cavendish/CavendishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windows33.Cavendish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavendishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
