19a1fe202457a7afac23896ecaa4fb3f26e306ec
박정훈 <ggongjja0@naver.com>
2023-09-21 17:44:07 +0900

    feat: 댓글 레이아웃 추가

--- frontend/src/components/BoardPage/BoardDetailComponent.jsx
+++ frontend/src/components/BoardPage/BoardDetailComponent.jsx
@@ -8,6 +8,14 @@ export default function BoardDetailComponent() {
       </div>
       <hr />
       <div className="detail_content">content</div>
+
+      <div className="comment_block">
+        <textarea className="comment_input" />
+        <button className="comment_input_button" type="button">
+          등록
+        </button>
+      </div>
+
       <div className="buttons">
         <Link className="button_link" to="/board">
           돌아가기
