87f0292246cc672a74a7541bcfdd1d1947134b52
김범창 <tigerspear.kim@gmail.com>
2023-09-21 09:57:36 +0900

    feat: swagger token 입력 추가

--- backend/src/main/java/com/windows33/cavendish/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/windows33/cavendish/global/config/SwaggerConfig.java
@@ -3,8 +3,12 @@ package com.windows33.cavendish.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,11 +20,15 @@ public class SwaggerConfig {
                 .version("v0.0.1")
                 .description("Cavendish 프로젝트의 API 명세서입니다.");
 
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(info);
     }
 
 }
-
-
