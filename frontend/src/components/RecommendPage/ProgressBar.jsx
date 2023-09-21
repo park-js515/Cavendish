58815998bde9c01221a8fde0a49789345ceb0c72
park-js515 <hdcom37@gmail.com>
2023-09-21 13:32:09 +0900

    feat : gitignore 수정 및 로 그인 api 초기 설정

--- frontend/src/components/RecommendPage/ProgressBar.jsx
+++ frontend/src/components/RecommendPage/ProgressBar.jsx
@@ -1,5 +1,21 @@
+const list = [{}, {}, {}, {}, {}, {}];
+
 const ProgressBar = () => {
-  return <>ProgressBar</>;
+  return (
+    <div className="progressBar">
+      <div className="steps">
+        <span className="circle active">1</span>
+        <span className="circle">2</span>
+        <span className="circle">3</span>
+        <span className="circle">4</span>
+        <span className="circle">5</span>
+        <span className="circle">!</span>
+        <div className="progress-bar">
+          <span className="indicator"></span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProgressBar;
