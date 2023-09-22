da1897b6cbf12efbb8b315572ad8b2e4352704e8
park-js515 <hdcom37@gmail.com>
2023-09-22 17:57:13 +0900

    feat : 추천 페이지 중간 정리

--- frontend/src/components/RecommendPage/Process/Sub/Process1/Process1_1.jsx
+++ frontend/src/components/RecommendPage/Process/Sub/Process1/Process1_1.jsx
@@ -1,24 +1,27 @@
 // 가지고 있는 부품이 있는가?
 const Process1_1 = ({ setSubProcess }) => {
   return (
-    <div className="sub proc1">
-      <div>
-        <div className="center">가지고 있는 부품이 있나요?</div>
-        <div className="center">
-          <button
+    <div className="sub">
+      <div className="jc">
+        <div className="text">가지고 있는 본체 부품이 있나요?</div>
+        <br />
+        <div className="btn-group">
+          <div
+            className="btn"
             onClick={() => {
               setSubProcess(1);
             }}
           >
-            Yes
-          </button>
-          <button
+            네
+          </div>
+          <div
+            className="btn"
             onClick={() => {
               setSubProcess(2);
             }}
           >
-            No
-          </button>
+            아니오
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/RecommendPage/Process/Sub/Process1/Process1_2.jsx
+++ frontend/src/components/RecommendPage/Process/Sub/Process1/Process1_2.jsx
@@ -1,12 +1,46 @@
 import { setTab } from "components/RecommendPage/Tab/TabGroup";
 // 9가지 부품 리스트
-const list = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+const list = [
+  {
+    name: "CPU",
+  },
+  { name: "메인보드" },
+  {
+    name: "메모리",
+  },
+  {
+    name: "그래픽카드",
+  },
+  {
+    name: "SSD",
+  },
+  {
+    name: "HDD",
+  },
+  {
+    name: "케이스",
+  },
+  {
+    name: "파워",
+  },
+  {
+    name: "쿨러",
+  },
+];
 
-const Process1_2 = ({setSubProcess}) => {
-  return <div className="sub proc2">
-    <p>Process1_2</p>
-    <button onClick={() => {setSubProcess(2)}}>버튼1</button>
-  </div>;
+const Process1_2 = ({ setSubProcess }) => {
+  return (
+    <div className="sub proc2">
+      <p>Process1_2</p>
+      <button
+        onClick={() => {
+          setSubProcess(2);
+        }}
+      >
+        버튼1
+      </button>
+    </div>
+  );
 };
 
 export default Process1_2;
