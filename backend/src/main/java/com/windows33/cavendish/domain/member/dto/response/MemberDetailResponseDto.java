9de2fa4698949719b419f70a333db535b3c76bf9
김범창 <tigerspear.kim@gmail.com>
2023-09-25 14:39:35 +0900

    feat: 역직렬화 문제 해결

--- backend/src/main/java/com/windows33/cavendish/domain/member/dto/response/MemberDetailResponseDto.java
+++ backend/src/main/java/com/windows33/cavendish/domain/member/dto/response/MemberDetailResponseDto.java
@@ -2,9 +2,11 @@ package com.windows33.cavendish.domain.member.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberDetailResponseDto {
 
     private String loginId;
