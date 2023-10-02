d7beb9b4a26a20264ef008606614be14cbe5f544
김범창 <tigerspear.kim@gmail.com>
2023-10-03 02:59:04 +0900

    feat: 부품 북마크 등록, 부품 북마크 목록 조회 기능 구현

--- backend/src/main/java/com/windows33/cavendish/domain/part_bookmark/service/PartBookmarkServiceImpl.java
+++ backend/src/main/java/com/windows33/cavendish/domain/part_bookmark/service/PartBookmarkServiceImpl.java
@@ -1,6 +1,8 @@
 package com.windows33.cavendish.domain.part_bookmark.service;
 
 import com.windows33.cavendish.domain.part_bookmark.dto.request.PartBookmarkAddRequestDto;
+import com.windows33.cavendish.domain.part_bookmark.entity.PartBookmark;
+import com.windows33.cavendish.domain.part_bookmark.repository.PartBookmarkRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PartBookmarkServiceImpl implements PartBookmarkService{
 
+    private final PartBookmarkRepository partBookmarkRepository;
+
     @Override
     public Integer addPartBookmark(PartBookmarkAddRequestDto partBookmarkAddRequestDto, Integer userId) {
+        PartBookmark.PartBookmarkBuilder partBookmark = PartBookmark.builder()
+                .name(partBookmarkAddRequestDto.getName())
+                .userId(userId)
+                .partCategory(partBookmarkAddRequestDto.getPartCategory())
+                .partId(partBookmarkAddRequestDto.getPartId());
 
-        return null;
+        Integer partBookmarkId = partBookmarkRepository.save(partBookmark.build()).getId();
+
+        return partBookmarkId;
     }
 
 }
